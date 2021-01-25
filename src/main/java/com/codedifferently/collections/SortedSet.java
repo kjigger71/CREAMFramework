9f7e3c3d481dde1ceb1b2342f9992c8593e71576
unknown <alvynkgillett@gmail.com>
2021-01-25 12:12:21 -0500

    framework

---
+++ src/main/java/com/codedifferently/collections/SortedSet.java
@@ -0,0 +1,39 @@
+package com.codedifferently.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SortedSet<T>{
+
+    private int size;
+    private ArrayList<T> list;
+
+
+ public boolean isEmpty() {
+     return false;
+ }
+
+    public int size(){
+        return 0;
+    }
+
+    public void clear(){
+
+    }
+    public Object[] toArray(){
+        return new Object[0];
+    }
+    public boolean Equals(){
+        return true;
+    }
+
+    public Object remove() {
+        return null;
+    }
+}
+
+
+
+
---
+++ src/test/java/com/codedifferently/collections/SortedSetTest.java
@@ -0,0 +1,37 @@
+package com.codedifferently.collections;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortedSetTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void toArray() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void remove() {
+    }
+}
